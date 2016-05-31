c5537d196faceaaac7d111fdb157ba7b58b92975
VadimSKY <vribak@gmail.com>
2016-05-31 12:03:35 +0300

    restructuring

---
+++ app/middleware/tests/middleware.spec.js
@@ -0,0 +1,14 @@
+/**
+ * Created by user on 31/05/2016.
+ */
+describe ("Middleware Services", function() {
+    it('The tests should be defined...', function() {
+        // ...
+    });
+    describe ("Authentication", function () {
+    });
+    describe ("Logger", function () {
+    });
+
+});
+
