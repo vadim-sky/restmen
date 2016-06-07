e3cbccb597dc55a89db8834dfe0c9ca8fe178a88
vadimsky <vribak@gmail.com>
2016-06-08 00:08:04 +0300

    create simple test

---
+++ tests/middleware/tests/middleware.spec.js
@@ -0,0 +1,9 @@
+describe("Middleware Services", function () {
+    it('The tests should be defined...', function () {
+    });
+    describe("Authentication", function () {
+    });
+    describe("Logger", function () {
+    });
+});
+//# sourceMappingURL=middleware.spec.js.map
